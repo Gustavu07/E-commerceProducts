12bbcd489d2123931c0aa62a6bfedac359267dd9
Gustavo <110668274+Gustavu07@users.noreply.github.com>
2025-10-13 15:08:38 -0400

    subiendo archivo de barril

---
+++ components/index.ts
@@ -0,0 +1,7 @@
+export * from './banner/Banner';
+export * from './layout/Footer';
+export * from './layout/Navbar';
+export * from './products/ProductCarousel';
+export * from './ui/button';
+export * from './ui/card';
+export * from './ui/carousel';
